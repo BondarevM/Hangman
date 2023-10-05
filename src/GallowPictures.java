3dfebaa6504f47e7eae0d5603d80dc314f2b03cb
Mikhail <bondarevmisha99@gmail.com>
2023-10-05 20:13:51 +0700

    project created

---
+++ src/GallowPictures.java
@@ -0,0 +1,30 @@
+public class GallowPictures {
+    public static void fristMisstake(){
+        System.out.println("__________\n|        |\n|        O\n|\n|\n|\n|");
+    }
+    public static void secondMisstake(){
+        System.out.println("__________\n|        |\n|        O\n|        |\n|        |\n|\n|" );
+    }
+    public static void thirdMisstake(){
+        System.out.println("__________\n|        |\n|        O\n|       /|\n|        |\n|\n|" );
+    }
+    public static void fourthMisstake(){
+        System.out.println("__________\n|        |\n|        O\n|       /|\\\n|        |\n|\n|" );
+    }
+    public static void fifthMisstake(){
+        System.out.println("__________\n|        |\n|        O\n|       /|\\\n|        |\n|       /\n|" );
+    }
+    public static void lastMisstake(){
+        System.out.println("__________\n|        |\n|        O\n|       /|\\    GAME OVER\n|        |\n|       / \\\n|" );
+    }
+
+
+    public static void main(String[] args) {
+        fristMisstake();
+        secondMisstake();
+        thirdMisstake();
+        fourthMisstake();
+        fifthMisstake();
+        lastMisstake();
+    }
+}
