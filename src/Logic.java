844897c6ca5f0a23cfa576edd5616c4d3af26cf6
Mikhail <bondarevmisha99@gmail.com>
2023-10-07 12:56:04 +0700

    работающая версия

--- src/Logic.java
+++ src/Logic.java
@@ -3,14 +3,34 @@ import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
+
+
 public class Logic {
     static public void disagree(){
         System.out.println("Пока пока!");
     }
+
+
+    public static void agree() throws java.io.FileNotFoundException {
+        String word = GettingRandomWord.GettingRandomWord();
+        System.out.println(word);
+        String mask = "*".repeat(word.length());
+        System.out.println("Ваше слово: " + mask);
+
+        Scanner scanner = new Scanner(System.in);
+        String answer = scanner.next();
+        if (answer.length()>1 || !answer.matches("[а-яА-Я]")){
+            System.out.println("Неккоректный ответ, введите букву!");
+
+        } else {
+            ;
+        }
+    }
+
 }
 
 class GettingRandomWord {
-    public static void GettingRandomWord() throws java.io.FileNotFoundException {
+    public static String GettingRandomWord() throws java.io.FileNotFoundException {
         File file = new File("Words");
         Scanner scanner = new Scanner(file);
         String worldsLine = scanner.nextLine();
@@ -18,13 +38,12 @@ class GettingRandomWord {
 
         Random random = new Random();
         int randomInxex = random.nextInt(wordsArray.length);
-
         String word = wordsArray[randomInxex];
-        System.out.println(word);
+        return word;
     }
 
 
     public static void main(String[] args) throws java.io.FileNotFoundException {
-        GettingRandomWord();
+
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,25 @@
 import java.util.Scanner;
 public class Main {
 
-    public static void Greetings(){
+    public static void greetings() throws java.io.FileNotFoundException{
         System.out.println("Вы хотите сыграть в игру? {Y}   {N}");
         startGame();
     }
-    public static void startGame() {
+    public static void startGame() throws java.io.FileNotFoundException {
         Scanner scanner = new Scanner(System.in);
         String answer = scanner.nextLine();
         if (answer.equals("N") || answer.equals("n")){
             Logic.disagree();
         } else if(answer.equals("Y") || answer.equals("y")){
-            System.out.println("Пошла игра");
+            Logic.agree();
         } else {
             System.out.println("Некорректный ответ");
-            Greetings();
+            greetings();
         }
         scanner.close();
     }
 
-    public static void main(String[] args) {
-    Greetings();
+    public static void main(String[] args) throws java.io.FileNotFoundException {
+        Logic.agree();
     }
 }
