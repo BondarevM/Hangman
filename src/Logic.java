1c4e743d59d16452419978ae8b87d0c3b2b785bf
Mikhail <bondarevmisha99@gmail.com>
2023-10-09 21:48:40 +0700

    working version

--- src/Logic.java
+++ src/Logic.java
@@ -1,5 +1,7 @@
 import java.io.File;
+import java.sql.SQLOutput;
 import java.util.Arrays;
+import java.util.Locale;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -13,7 +15,7 @@ public class Logic {
 
     public static String agree() throws java.io.FileNotFoundException {
         String word = GettingRandomWord.GettingRandomWord();
-        System.out.println(word);
+//        System.out.println(word);
         return word;
 
 
@@ -21,20 +23,48 @@ public class Logic {
 
     public static void game(String word) throws java.io.FileNotFoundException{
         System.out.println(word);
+        String mask = "*".repeat(word.length());
+        String maskAfter = "";
+
+        System.out.println("Ваше слово: " + mask);
+        System.out.println("Введите букву");
         int counter = 0;
-
         Scanner scanner = new Scanner(System.in);
-        String answer = scanner.next();
+        String answer = scanner.next().toLowerCase(Locale.ROOT);
+        System.out.println(answer);
 
-
-
+        String used = "аоуюои";
 
         if (answer.length()>1 || !answer.matches("[а-яА-Я]")){
-            System.out.println("Неккоректный ответ, введите букву!");
+            System.out.println("Неккоректный ответ, введите букву на кириллице!");
+        } else if (answer.contains(answer) && !used.contains(answer)){
+            used +=answer;
+            int indexToReplace = word.indexOf(answer);
+            mask = mask.substring(0, indexToReplace) + answer + mask.substring(indexToReplace+1);
+            System.out.println(indexToReplace);
+            game(word);
+            right();
+        } else if (used.contains(answer)) {
+            System.out.println("Вы уже использовали данную букву!");
+            game(word);
+        } else {
+            wrond();
         }
     }
 
+    public static void right(){
+        System.out.println("right");
+
+    }
+
+    public static void wrond(){
+        System.out.println("wrong");
+    }
+
 }
+
+
+
 
 class GettingRandomWord {
     public static String GettingRandomWord() throws java.io.FileNotFoundException {
